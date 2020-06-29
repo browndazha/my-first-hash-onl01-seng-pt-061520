b1882fdfbd8959241c14465441dc8f163b53e398
Dazha Brown <dajahbrown@hotmail.com>
2020-06-29 05:00:54 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -23,17 +23,14 @@ def retrieval
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-
+end
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
-end
+  shipping_manifest["oil paintings"]
+  end
 
-def adding
-  shipping_manifest = {
-                  "whale bone corsets" => 5,
-                  "porcelain vases" => 2,
-                  "oil paintings" => 3
-                 }
+  def adding
+    
   # add 2 muskets to the shipping_manifest hash below
 
 
