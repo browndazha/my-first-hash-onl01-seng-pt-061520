a11bb6dde5dc4d1d8912376bb49ece410d2a867e
Dazha Brown <dajahbrown@hotmail.com>
2020-06-29 05:06:19 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -29,7 +29,10 @@ def retrieval
 
 def adding
   shipping_manifest = {
-  
+                    "whale bone corsets" => 5,
+                    "porcelain vases" => 2,
+                    "oil paintings" => 3
+                   }
 
   # add 4 gun powder to the shipping_manifest hash below
 
